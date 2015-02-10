58fb2e3d52b21b3fdfe6faafe49f6ce908879dba
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-02-10 20:27:43 +0900

    Having some failure

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
+import securesocial.core.{UserId, Identity, UserServicePlugin, AuthenticationMethod}
+import securesocial.core.SecureSocial
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+object Application extends Controller with securesocial.core.SecureSocial {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = UserAwareAction { implicit request =>
+    val userName = request.user match {
+      case Some(user) => user.fullName
+      case _ => "guest"
+    }
+    Ok(views.html.index("Hello %s.".format(userName)))
   }
 
 }
