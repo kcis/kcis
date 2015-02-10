991139a102f6ff1acd22f5cf405036eea60cd86b
func <t.matsudate@gmail.com>
2015-02-11 02:09:42 +0900

    以下二箇所の修正: 参照する実テーブル名を大文字から小文字へ 施設情報のカラムを追加

--- app/models/aged.scala
+++ app/models/aged.scala
@@ -1,17 +1,22 @@
 package models
 import scala.slick.driver.MySQLDriver.simple._
-import models.insurance
-import java.util.Date
+import models.insurance._
+import java.sql.Date
 
-class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, Int)](tag, "AGED") {
-  def id = column[Int]("AGED_ID", O.PrimaryKey)
-  def name = column[String]("NAME")
-  def kana = column[String]("KANA")
-  def age = column[Byte]("AGE")
-  def sex = column[Char]("SEX")
-  def birthed = column[Date]("BIRTHED")
-  def insuranceId = column[Int]("INSURANCE_ID")
+class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, Int)](tag, "aged") {
+  def id = column[Int]("aged_id", O.PrimaryKey)
+  def name = column[String]("name")
+  def kana = column[String]("kana")
+  def age = column[Byte]("age")
+  def sex = column[Char]("sex")
+  def birthed = column[Date]("birthed")
+  def address = column[String]("address")
+  def postal = column[String]("postal")
+  def phone = column[String]("phone")
+  def insuranceId = column[Int]("insrance_id")
+  def homeId = column[Int]("home_id")
+  def left = column[Date]("left")
   def * = (id, name, kana, age, sex, birthed, insuranceId)
-  def insurance = foreignKey("INSURANCE_FK", insuranceId, insrances)(_.id)
+  def insurance = foreignKey("insrance_fk", insuranceId, insrances)(_.id)
 }
 val aged = TableQuery[Aged]
