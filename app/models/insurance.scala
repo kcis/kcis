b91af4bf4b12c99743f6e946e74306848d5b7606
func <t.matsudate@gmail.com>
2015-02-12 16:07:10 +0900

    Fixed a definition of wildcard selection.

--- app/models/insurance.scala
+++ app/models/insurance.scala
@@ -7,7 +7,7 @@ class Insurances(tag: Tag) extends Table[(Int, Byte, Date, Date)](tag, "insuranc
   def started = column[Date]("started")
   def expired = column[Date]("expired")
   def agedId = column[Int]("aged_id")
-  def * = (id, nursingCareLevel, started, expired)
+  def * = (id, nursingCareLevel, started, expired, agedId)
   def aged = foreignKey("aged_fk", agedId, aged)(_.id)
 }
 val insurances = TableQuery[Insurances]
