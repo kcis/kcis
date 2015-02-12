57b7da9a07d1df79a0f8150aa014e3d3d9148789
func <t.matsudate@gmail.com>
2015-02-13 03:47:04 +0900

    Fixed: A definition of selection by wild-card. (施設の情報や介護保険の情報が欲しい時は、後で必要に応じてテーブルを結合すればよい)

--- app/models/aged.scala
+++ app/models/aged.scala
@@ -2,7 +2,7 @@ package models
 import scala.slick.driver.MySQLDriver.simple._
 import java.sql.Date
 
-class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, Int)](tag, "aged") {
+class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, String, String, String, Int, Int, Date)](tag, "aged") {
   def id = column[Int]("aged_id", O.PrimaryKey)
   def name = column[String]("name")
   def kana = column[String]("kana")
@@ -15,8 +15,8 @@ class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, Int)]
   def insuranceId = column[Int]("insurance_id")
   def homeId = column[Int]("home_id")
   def left = column[Date]("left")
-  def * = (id, name, kana, age, sex, birthed, address, postal, phone, insuranceId, homeId, left)
+  def * = (id, name, kana, age, sex, birthed, address, postal, phone, left)
   def insurance = foreignKey("insurance_fk", insuranceId, insurances)(_.id)
   def home = foreignKey("home_fk", homeId, homes)(_.id)
 }
-val aged = TableQuery[Aged]
+val ageds = TableQuery[Ageds]
