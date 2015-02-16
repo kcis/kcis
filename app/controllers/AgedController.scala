edb92530ba09004c48da010e789dd6f9951de41b
func <t.matsudate@gmail.com>
2015-02-16 19:46:48 +0900

    Fixed: コントローラ名の変更

--- app/controllers/AgedController.scala
+++ app/controllers/AgedController.scala
@@ -3,7 +3,7 @@ import play.api.db.slick._
 import play.api.mvc._
 import models._
 
-object Aged extends Controller {
+object AgedController extends Controller {
   def index = DBAction { implicit request =>
     Ok(views.html.Aged.index(request.session, Aged.getAgedForIndex))
   }
