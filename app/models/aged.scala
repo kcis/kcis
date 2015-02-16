c7e6d7d993cfd71adf4ead1927229aaf37b24c56
func <t.matsudate@gmail.com>
2015-02-16 19:12:47 +0900

    Fixed: TableQuery を val フィールドへ切り離した。

--- app/models/aged.scala
+++ app/models/aged.scala
@@ -23,7 +23,8 @@ class Aged(tag: Tag) extends Table[(Int, String, String, Byte, Char, Date, Strin
   def agedIndex = index("agedIndex", id)
 }
 
-object aged extends TableQuery[Aged] {
-  //override def leftJoin[E2, U2, D[_]](q2: Query[E2, U2, D]): BaseJoinQuery[E, E2, E.TableElementType, U2, Seq] = super.leftJoin(q2)
-  def getAgedForIndex = (this leftJoin insurances on (_.insuranceId === _.id)).map { case (a, i) => (a.id, a.name, a.kana, a.age, a.sex, a.birthed, i.expired.?) }.run
+val aged = TableQuery[Aged]
+
+object Aged {
+  def getAgedForIndex = (aged leftJoin insurances on (_.insuranceId === _.id)).map { case (a, i) => (a.id, a.name, a.kana, a.age, a.sex, a.birthed, i.expired.?) }.run
 }
