d15f055200d4ecf37e7be3c222dd744406dc6f63
func <t.matsudate@gmail.com>
2015-02-16 15:20:59 +0900

    Fixed: Column constraints; (長さとNotNull制約)

--- app/models/homes.scala
+++ app/models/homes.scala
@@ -1,12 +1,12 @@
 package models
-
 import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.ast.ColumnOption.NotNull
 
 class Homes(tag: Tag) extends Table[(Int, String, String)](tag, "homes") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-  def corporationName = column[String]("corporationName")
-  def officeName = column[String]("officeName")
-  def careType = column[String]("careType")
+  def corporationName = column[String]("corporationName", O.Length(255, false), NotNull)
+  def officeName = column[String]("officeName", O.Length(255, false), NotNull)
+  def careType = column[String]("careType", O.Length(255, false), NotNull)
   def * = (id, corporationName, officeName, careType)
 
   def indexHomes = index("indexHomes", id)
