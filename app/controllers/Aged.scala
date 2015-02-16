ec2b9b8dd340f6f9382c4b7cda85d6ec47640770
func <t.matsudate@gmail.com>
2015-02-16 18:02:53 +0900

    Fixed: クエリ実行のメソッドチェインをモデル(aged)側に実装されたラッピングメソッドに書き換えた。

--- app/controllers/Aged.scala
+++ app/controllers/Aged.scala
@@ -5,14 +5,6 @@ import models._
 
 object Aged extends Controller {
   def index = DBAction { implicit request =>
-    val result =
-      (aged leftjoin insurances on (_.insuranceId === _.id))
-      .filter(a => a.homeId === request.sessiong.get("homeId"))
-      .map
-      {
-        case (a, i) => (a.id, a.name, a.kana, a.age, a.sex, a.birthed, i.expired.?)
-      }
-      .run
-    Ok(views.html.Ageds.index(request.session, result))
+    Ok(views.html.Ageds.index(request.session, aged.getAgedForIndex))
   }
 }
