3a676108fc03832c3a0b59daa18de1ba4c15bd40
func <t.matsudate@gmail.com>
2015-02-16 14:32:34 +0900

    Revised: Indentations.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,18 +2,19 @@
 
 CREATE TABLE aged
 (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    kana TINYTEXT CHARACTER SET utf8 COLATTE utf8_general_ci NOT NULL,
-    age TINYINT UNSIGNED NOT NULL,
-    sex TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    birthed TIMESTAMP NOT NULL,
-    address TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    postal TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    phone TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    insuranceId INT UNSIGNED NOT NULL,
-    homeId INT UNSIGNED NOT NULL,
-    left TIMESTAMP,
+    id          INT         UNSIGNEDPRIMARY KEY AUTO_INCREMENT,
+    name        TINYTEXT    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    kana        TINYTEXT    CHARACTER SET utf8 COLATTE utf8_general_ci NOT NULL,
+    age         TINYINT     UNSIGNED NOT NULL,
+    sex         TINYTEXT    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    birthed     TIMESTAMP   NOT NULL,
+    address     TEXT        CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    postal      TINYTEXT    CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    phone       TEXT        CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    insuranceId INT         UNSIGNED NOT NULL,
+    homeId      INT         UNSIGNED NOT NULL,
+    left        TIMESTAMP,
+
     CONSTRAINT insuranceFK FOREIGN KEY (insuranceId) REFERENCES insurances (id),
     CONSTRAINT homeFK FOREIGN KEY (homeId) REFERENCES homes (id),
     INDEX agedIndex (id)
