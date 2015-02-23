ce53d6826c40ad5bb1528825678a16c5f48d9a33
func <t.matsudate@gmail.com>
2015-02-24 01:14:35 +0900

    Added: アクセス権の管理のために必要な、ユーザ単位の役職を保存するテーブル。(医師とか介護員とか、そういうの)

---
+++ app/models/roles.scala
@@ -0,0 +1,16 @@
+package models
+import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.ast.ColumnOption.NotNull
+
+class Roles(tag: Tag) extends Table[](tag, "ROLES")
+{
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def role = column[String]("ROLE", NotNull)
+
+  def * = (id, role)
+}
+
+object Roles
+{
+  val roles = TableQuery[Roles]
+}
