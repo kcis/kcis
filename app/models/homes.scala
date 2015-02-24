86adca0021da0e826cfe30519bf504f0001d5bc1
func <t.matsudate@gmail.com>
2015-02-24 16:24:07 +0900

    Deleted: 文字列長の制約(なくても問題ない気がしてきた…)

--- app/models/homes.scala
+++ app/models/homes.scala
@@ -4,9 +4,9 @@ import scala.slick.ast.ColumnOption.NotNull
 
 class Homes(tag: Tag) extends Table[(Int, String, String, String)](tag, "HOMES") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-  def corporationName = column[String]("CORPORATION_NAME", O.Length(255, false), NotNull)
-  def officeName = column[String]("OFFICE_NAME", O.Length(255, false), NotNull)
-  def careType = column[String]("CARE_TYPE", O.Length(255, false), NotNull)
+  def corporationName = column[String]("CORPORATION_NAME", NotNull)
+  def officeName = column[String]("OFFICE_NAME", NotNull)
+  def careType = column[String]("CARE_TYPE", NotNull)
   def * = (id, corporationName, officeName, careType)
 
   def indexHomes = index("HOMES_INDEX", id)
