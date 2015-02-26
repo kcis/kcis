3abfef6e807fa7edc3b7ea06e776af4ff404af4b
func <t.matsudate@gmail.com>
2015-02-26 16:00:03 +0900

    Fixed: メソッド内のコードを可能な限り綺麗にするため、重複するコードを局所化した。

--- app/models/insurances.scala
+++ app/models/insurances.scala
@@ -16,31 +16,76 @@ class Insurances(tag: Tag) extends Table[(Int, Byte, Date, Date, Int)](tag, "INS
 
 object Insurances {
   val insurances = TableQuery[Insurances]
-  def createInsurance(id: Int, nursingCareLevel: Byte, started: Date)(implicit session: Session)
+
+  private def checkNextTerm(started: Date): Date
   {
     /*
-     * 介護保険の適用期限は登録(更新)から半年後の末日である。
-     * また、保険の更新を行うために必要な市役所は、遅くとも午後6時には閉まる。
+     * 介護保険の更新期限は、
+     * 初回: 認定された日から半年後の月末まで
+     * 2回目以降: 前回の更新が適用され始めた日から1年後の月末まで
      */
-    if (id > 0 && id <= Integer.MAX_VALUE)
+    val expired = Calendar.getInstance()
+    tmp.setTime(started)
+    val today = Calendar.getInstance()
+    today.setTime(new Date(System.currentTimeMillis()))
+
+    if (today.get(Calendar.YEAR) == expired.get(Calendar.YEAR) && today.get(Calendar.MONTH) - expired.get(Calendar.MONTH) < 6)
     {
-      val expired = Calendar.getInstance()
-      expired.setTime(started)
       expired.add(Calendar.MONTH, 6)
       expired.set(Calendar.DATE, expired.getActualMaximum(Calendar.DATE))
-      expired.set(Calendar.HOUR_OF_DAY, 18)
-      insurances.map(i => (i.id, i.nursingCareLevel, i.started, i.expired)).insert(id, nursingCareLevel, started, expired.getTime())
+      return expired.getTime()
     }
-    else throw new IllegalArgumentException()
+    else
+    {
+      expired.add(Calendar.YEAR, 1)
+      expired.set(Calendar.DATE, expired.getActualMaximum(Calendar.DATE))
+      return expired.getTime()
+    }
   }
+
+  // deleteInsurance() 用のチェッカ
+  private def doAfterChecking(id: Int, f: => Int)
+  {
+    if (id > 0) f
+    else throw new IllegalArgumentException("ID は 1 から Intの最大値(約21億) の範囲でなければなりません。")
+  }
+
+  // createInsurance() および updateInsurance() 用のチェッカ
+  private def doAfterChecking(id: Int, nursingCareLevel: Byte, f: => Int)
+  {
+    if (id > 0 && nursingCareLevel > 0 && nursingCareLevel <= 5) f
+    else
+    {
+      if (id <= 0) throw new IllegalArgumentException("IDは 1 から Intの最大値(約21億) の範囲でなければなりません。")
+      else throw new IllegalArgumentException("要介護度は 1 から 5 の範囲でなければなりません。")
+    }
+  }
+
+  def createInsurance(id: Int, nursingCareLevel: Byte, started: Date)(implicit session: Session)
+  {
+    doAfterChecking
+    (
+      id,
+      nursingCareLevel,
+      {
+        val expired = checkNextTerm(started)
+        insurances.map(i => (i.id, i.nursingCareLevel, i.started, i.expired)).insert(id, nursingCareLevel, started, expired)
+      }
+    )
+  }
+
   def updateInsurance(id: Int, nursingCareLevel: Byte, started: Date)(implicit session: Session)
   {
-    val expired = Calendar.getInstance()
-    expired.setTime(started)
-    expired.add(Calendar.MONTH, 6)
-    expired.set(Calendar.DATE, expired.getActualMaximum(Calendar.DATE))
-    expired.set(Calendar.HOUR_OF_DAY, 18)
-    insurances.filter(i => i.id === id).map(i => (i.nursingCareLevel, i.started, i.expired)).update(nursingCareLevel, started, expired.getTime())
+    doAfterChecking
+    (
+      id,
+      nursingCareLevel,
+      {
+        val expired = checkNextTerm(started)
+        insurances.filter(i => i.id === id).map(i => (i.nursingCareLevel, i.started, i.expired)).update(nursingCareLevel, started, expired)
+      }
+    )
   }
-  def deleteInsurance(id: Int)(implicit session: Session) = insurances.filter(i => i.id === id).delete
+
+  def deleteInsurance(id: Int)(implicit session: Session) = doAfterChecking(id, {insurance.filter(i => i.id === id).delete})
 }
