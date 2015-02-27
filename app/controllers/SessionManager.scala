da0e2024526503779846b63b0426fd3b2b08aac1
func <t.matsudate@gmail.com>
2015-02-28 01:10:46 +0900

    Added: ログイン関係のセッションを管理するクラス

---
+++ app/controllers/SessionManager.scala
@@ -0,0 +1,14 @@
+package controllers
+import play.api.db.slick._
+import play.api._
+import play.api.mvc._
+
+object SessionManager
+{
+  def isAuthorized(implicit session: Session): Bool = !(session.get("homeId").isEmpty && session.get("userId") && session.get("roleId").isEmpty)
+  def createLoginSession(homeId: Int, userId: Int, roleId: Int)(implicit session: Session) = session + ("homeId", Integer.toString(homeId)) + ("userId", Integer.toString(userId)) + ("roleId", Integer.toString(roleId))
+  def deleteLoginSession(implicit session: Session) = session - ("homeId") - ("userId") - ("roleId")
+  // まだログインしていない状態でログイン以外の画面を開いた時に、ログイン画面に飛ばしつつも元々開こうとしていた画面を覚えておくためのもの
+  def redirectFrom(uri: String)(implicit session: Session) = session + ("redirectFrom", uri)
+  def authorized(implicit session: Session) = session - ("redirectFrom")
+}
