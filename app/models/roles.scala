1208372be57cdff282838d2ba08261dc83ad644f
func <t.matsudate@gmail.com>
2015-02-27 22:10:50 +0900

    Fixed: case class を追加し、その周辺のコードを直した。

--- app/models/roles.scala
+++ app/models/roles.scala
@@ -2,15 +2,21 @@ package models
 import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.ast.ColumnOption.NotNull
 
-class Roles(tag: Tag) extends Table[(Int, String)](tag, "ROLES")
+case class Role(role: String)
+
+class Roles(tag: Tag) extends Table[(Int, Role)](tag, "ROLES")
 {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def role = column[String]("ROLE", NotNull)
 
-  def * = (id, role)
+  def * = (role) <> (Role.tupled, Role.unapply)
 }
 
 object Roles
 {
   val roles = TableQuery[Roles]
+
+  def createRoles(role: Roll)(implicit session: Session) = roles.insert(role)
+  def updateRoles(id: Int, role: Roll)(implicit session: Session) = roles.filter(r => r.id === id).update(role)
+  def deleteRoles(id: Int)(implicit session: Session) = roles.filter(r => r.id === id).delete
 }
