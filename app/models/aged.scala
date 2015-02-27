20bba11061bbea488044d699aafea25486421ee0
func <t.matsudate@gmail.com>
2015-02-27 18:38:09 +0900

    Added: 利用者テーブルへのデータを追加・更新・削除するメソッド

--- app/models/aged.scala
+++ app/models/aged.scala
@@ -4,9 +4,9 @@ import scala.slick.ast.ColumnOption.NotNull
 import java.sql.Date
 import java.util.Calendar
 
-case class Aged(id: Int, name: String, kana: String, age: Byte, sex: String, birthed: Date, address: String, postal: String, phone: String, insuranceId: Int, homeId: Int, left: Date)
+case class Aged(name: String, kana: String, age: Byte, sex: String, birthed: Date, address: String, postal: String, phone: String, insuranceId: Int, homeId: Int, left: Date)
 
-class Ageds(tag: Tag) extends Table[Aged](tag, "AGEDS")
+class Ageds(tag: Tag) extends Table[(Int, Aged)](tag, "AGEDS")
 {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def name = column[String]("NAME", NotNull)
@@ -20,7 +20,7 @@ class Ageds(tag: Tag) extends Table[Aged](tag, "AGEDS")
   def insuranceId = column[Int]("INSURANCE_ID", NotNull)
   def homeId = column[Int]("HOME_ID", NotNull)
   def left = column[Date]("LEFT")
-  def * = (id, name, kana, age, sex, birthed, address, postal, phone, insuranceId, homeId, left) <> (Aged.tupled, Aged.unapply)
+  def * = (name, kana, age, sex, birthed, address, postal, phone, insuranceId, homeId, left) <> (Aged.tupled, Aged.unapply)
 
   def insurance = foreignKey("INSURANCE_FK", insuranceId, Insurances.insurances)(_.id, onUpdate = ForeignKeyAction.Cascade, onDelete = ForeignKeyAction.Restrict)
   def home = foreignKey("HOME_FK", homeId, Homes.homes)(_.id, onUpdate = ForeignKeyAction.Cascade, onDelete = ForeignKeyAction.Restrict)
@@ -33,18 +33,26 @@ object Ageds
 
   private def doAfterChecking(aged: Aged, f: => T)
   {
-    val tmp = Calendar.getInstance()
-    tmp.setTime(aged.birthed)
+    val birthed = Calendar.getInstance()
+    birthed.setTime(aged.birthed)
+
     val today = Calendar.getInstance()
     today.setTime(new Date(System.currentTimeMillis()))
 
-    if (aged.age <= 0) throw new IllegalArgumentException("年齢には自然数を入力してください。")
-    else if (!(aged.sex.equals("男性") || aged.sex.equals("女性"))) throw new IllegalArgumentException("性別は男性か女性のいずれか1つです。")
-    else if (today.get(Calendar.YEAR) - tmp.get(Calendar.YEAR) > Byte.MAX_BALUE) throw new IllegalArgumentException("生年月日は今から127年前までの範囲で入力してください。")
+    // Slick のテーブル定義は、広範なデータベースに共通する以上のオプションは提供できない。
+    // なので、負数を認めてはいけない場合、このようにして外側から検査する必要がある。
+    if (aged.age < 0) throw new IllegalArgumentException("年齢には0以上127以下の数字を入力してください。")
+    else if (aged.birthed.after(today.getTime())) throw new IllegalArgumentException("生年月日は現在よりも前の日付で入力してください。")
+    else if (aged.age != (today.get(Calendar.YEAR) - birthed.get(Calendar.YEAR))) throw new IllegalArgumentException("年齢と生年月日が一致していません。")
     else f
   }
 
+  // 利用者一覧に使うデータはこれ一つでまとめて表示できる
   def getAgedForIndex(implicit session: Session) = (ageds leftJoin Insurances.insurances on (_.insuranceId === _.id)).map{case (a, i) => (a.id, a.name, a.kana, a.age, a.sex, a.birthed, i.expired.?)}.run
   
-  def createAged(aged: Aged)(implicit session: Session) = ageds.map(a => a.name, a.kana, a.age, a.sex, a.birthed, a.address, a.postal, a.phone, a.insuranceId, a.homeId, a.left).insert(aged.name, aged.kana, aged.age, aged.sex, aged.birthed, aged.address, aged.postal, aged.phone, aged.insuranceId, aged.homeId, aged.left)
+  def createAged(aged: Aged)(implicit session: Session) = doAfterChecking(aged, {ageds.insert(aged)})
+
+  def updateAged(id: Int, aged: Aged)(implicit session: Session) = doAfterChecking(aged, {ageds.filter(a => a.id === id).update(aged)})
+
+  def deleteAged(id: Int)(implicit session: Session) = ageds.filter(a => a.id === id).delete
 }
