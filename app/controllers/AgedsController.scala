8af751b8ff632a769a7d3023e1061c26b0c6a996
func <t.matsudate@gmail.com>
2015-03-02 02:24:35 +0900

    Fixed and Added: TableQuery プライベート化の影響箇所と、ステーションID(このテーブルではhome(施設)IDと読んでいる)をセッションから正しく読み取るためのラッパメソッド

--- app/controllers/AgedsController.scala
+++ app/controllers/AgedsController.scala
@@ -5,6 +5,6 @@ import models._
 
 object AgedController extends Controller {
   def index = DBAction { implicit request =>
-    Ok(views.html.Ageds.index(request.session, Ageds.getAgedForIndex))
+    Ok(views.html.Ageds.index(request.session, Ageds.joinForLists))
   }
 }
--- app/controllers/SessionManager.scala
+++ app/controllers/SessionManager.scala
@@ -6,14 +6,20 @@ import play.api.mvc._
 object SessionManager
 {
   def isAuthorized(implicit session: Session): Bool = !(session.get("homeId").isEmpty && session.get("userId") && session.get("roleId").isEmpty)
+
   def createLoginSession(homeId: Int, userId: Int, roleId: Int)(implicit session: Session) = session + ("homeId", Integer.toString(homeId)) + ("userId", Integer.toString(userId)) + ("roleId", Integer.toString(roleId))
+
   def deleteLoginSession(implicit session: Session) = session - ("homeId") - ("userId") - ("roleId")
   // まだログインしていない状態でログイン以外の画面を開いた時に、ログイン画面に飛ばしつつも元々開こうとしていた画面を覚えておくためのもの
+
   def redirectFrom(uri: String)(implicit session: Session) = session + ("redirectFrom", uri)
+
   def authorized(implicit session: Session)
   {
     val redirectFrom = session.get("redirectFrom")
     session - ("redirectFrom")
     redirectFrom
   }
+
+  def getHomeId(implicit session: Session) = session.get("homeId")
 }
