ef49fe8c4d71a34cb2c55b6b128ca37de06a4f2d
func <t.matsudate@gmail.com>
2015-03-04 01:53:08 +0900

    注意: 書きかけ(眠いので一旦寝る)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,36 @@
 package controllers
-
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 
-object Application extends Controller {
+case class LoginParameter(stationId: Int, memberId: String, password: String)
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+object Application extends Controller
+{
+
+  def index = WithAuthorization
+  {
+    Redirect("/aged", 302)
   }
 
+  def login = Action
+  {
+    if (SessionManager.isAuthorized) NotModified else Ok(views.html.login)
+  }
+
+  def auth = Action
+  {
+    val authForm: Form[LoginParameter] = Form
+    (
+      mapping
+      (
+        "stationId" -> text,
+        "memberId" -> text,
+        "password" -> text
+      )(LoginParameter.apply)(LoginParameter.unapply)
+    )
+    val loginParameter = authForm.bindFormRequest.get
+    
+  }
 }
